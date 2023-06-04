2d3b24eb58971c777bb7e91c6e024079e37fbcab
David Lee <ing.jdplee@gmail.com>
2023-06-04 09:38:57 -0400

    Step: Upload modal and functionality

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,8 +3,24 @@
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
+
 const Library = () => {
-  const onClick = () => {};
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+
+  const onClick = () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+    // TODO: Check for subscription
+
+    return uploadModal.onOpen();
+  };
+
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-between px-5 pt-4">
